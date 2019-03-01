2665428500cfb3254f44f92a2de68ed03413e4ce
rripio <filarete@gmail.com>
2019-03-01 11:25:34 +0100

    minor correction

--- src/smoothlog.m
+++ src/smoothlog.m
@@ -38,7 +38,7 @@ function xs=smoothlog(x,ppo,ppoSm);
 
     if w<3
         error("Smoothing step must at least tree times greater than frequency step.\n");
-    elseif (ppoSm <= 0)|(ppo <= 0) 
+    elseif (ppoSm <= 0) || (ppo <= 0) 
         error("Steps must be positive");
     end
     
